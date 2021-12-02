3d4fdd0a9a1cb45ae46c7d7e670d5593c1481210
Sherman <shaopengyu@rt-thread.com>
2021-12-02 15:53:11 +0800

    [fix] 修改 rw007 引脚宏定义

--- bsp/ra6m4-cpk/ports/rw007/drv_rw007.c
+++ bsp/ra6m4-cpk/ports/rw007/drv_rw007.c
@@ -11,22 +11,22 @@ extern void spi_wifi_isr(int vector);
 static void rw007_gpio_init(void)
 {
     /* Configure IO */
-    rt_pin_mode(RW007_RST_PIN, PIN_MODE_OUTPUT);
-    rt_pin_mode(RW007_INT_BUSY_PIN, PIN_MODE_INPUT_PULLDOWN);
+    rt_pin_mode(RA_RW007_RST_PIN, PIN_MODE_OUTPUT);
+    rt_pin_mode(RA_RW007_INT_BUSY_PIN, PIN_MODE_INPUT_PULLDOWN);
 
     /* Reset rw007 and config mode */
-    rt_pin_write(RW007_RST_PIN, PIN_LOW);
+    rt_pin_write(RA_RW007_RST_PIN, PIN_LOW);
     rt_thread_delay(rt_tick_from_millisecond(100));
-    rt_pin_write(RW007_RST_PIN, PIN_HIGH);
+    rt_pin_write(RA_RW007_RST_PIN, PIN_HIGH);
 
     /* Wait rw007 ready(exit busy stat) */
-    while (!rt_pin_read(RW007_INT_BUSY_PIN))
+    while (!rt_pin_read(RA_RW007_INT_BUSY_PIN))
     {
         rt_thread_delay(5);
     }
 
     rt_thread_delay(rt_tick_from_millisecond(200));
-    rt_pin_mode(RW007_INT_BUSY_PIN, PIN_MODE_INPUT_PULLUP);
+    rt_pin_mode(RA_RW007_INT_BUSY_PIN, PIN_MODE_INPUT_PULLUP);
 }
 
 static struct rt_spi_device rw007_dev;
@@ -34,10 +34,10 @@ static struct rt_spi_device rw007_dev;
 int wifi_spi_device_init(void)
 {
     char sn_version[32];
-    uint32_t cs_pin = RW007_CS_PIN;
+    uint32_t cs_pin = RA_RW007_CS_PIN;
 
     rw007_gpio_init();
-    rt_hw_spi_device_attach(&rw007_dev, "wspi", RW007_SPI_BUS_NAME, (void *)cs_pin);
+    rt_hw_spi_device_attach(&rw007_dev, "wspi", RA_RW007_SPI_BUS_NAME, (void *)cs_pin);
     rt_hw_wifi_init("wspi");
 
     rt_wlan_set_mode(RT_WLAN_DEVICE_STA_NAME, RT_WLAN_STATION);
@@ -60,8 +60,8 @@ static void int_wifi_irq(void *p)
 
 void spi_wifi_hw_init(void)
 {
-    rt_pin_attach_irq(RW007_INT_BUSY_PIN, PIN_IRQ_MODE_FALLING, int_wifi_irq, 0);
-    rt_pin_irq_enable(RW007_INT_BUSY_PIN, RT_TRUE);
+    rt_pin_attach_irq(RA_RW007_INT_BUSY_PIN, PIN_IRQ_MODE_FALLING, int_wifi_irq, 0);
+    rt_pin_irq_enable(RA_RW007_INT_BUSY_PIN, RT_TRUE);
 }
 
 #endif /* BSP_USING_RW007 */
